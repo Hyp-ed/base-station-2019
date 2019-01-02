2a98ba64f186a3e57658e07cdf3e7bcd9fc3632b
Neil Weidinger <neilweidingern@gmail.com>
2019-01-02 22:14:47 +0100

    post request to start server should be idempotent, returns status of whether server is running or not

--- src/main/java/server/Controller.java
+++ src/main/java/server/Controller.java
@@ -2,12 +2,28 @@ package server;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class Controller {
 
+    private Server server;
+    private boolean serverRunning = false;
+
     @RequestMapping("/")
     public String index() {
-        return "Don't step on snek!";
+        return "HOMEPAGE!!!";
+    }
+
+    @RequestMapping(path="/server", method=RequestMethod.POST)
+    public boolean initiateServer() {
+        if (!serverRunning) {
+            server = new Server();
+            Thread serverThread = new Thread(server);
+            serverThread.start();
+            serverRunning = true;
+        }
+
+        return serverRunning;
     }
 }
