283e310db5a82e20a79ed71d9ccd35be3d539214
Neil Weidinger <neilweidingern@gmail.com>
2019-01-04 18:12:36 +0100

    really basic (read: crappy) websocket implementation that I basically copied from a tutorial lol; removed index method in controller bc for some reason the html and js wasn't loading, honestly idk how front end with spring works and tbh spring is so annoyingly confusing

--- src/main/java/server/Controller.java
+++ src/main/java/server/Controller.java
@@ -4,15 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 @RestController
 public class Controller {
     private Server server;
 
-    @RequestMapping("/")
-    public String index() {
-        return "HOMEPAGE!!!";
-    }
+    // @RequestMapping("/")
+    // public String index() {
+        // return "HOMEPAGE!!!";
+    // }
 
     // returns true if server has connected, false if not/server isn't even running
     @RequestMapping(path = "/server", method = RequestMethod.POST)
@@ -33,5 +35,15 @@ public class Controller {
         }
 
         return ResponseEntity.badRequest().body(null);
+    }
+
+    @MessageMapping("/data")
+    @SendTo("/topic/pod_stats")
+    public String podStats(String whatever) {
+        if (server != null && server.isConnected()) {
+            return server.getCmd() + " --- " + server.getData();
+        }
+
+        return "server not started or summin";
     }
 }
